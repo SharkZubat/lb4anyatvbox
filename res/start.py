53b854e3a4fde4d59169ac9cbfa0bc214b2f8f23
SharkZubat <akula.puppet.zaba123@gmail.com>
2025-08-26 18:01:57 +0300

    update

--- res/start.py
+++ res/start.py
@@ -1,4 +1,32 @@
 import os
+import configparser
+import json
 
-os.system("clear")
-print("placeholder")
+config = configparser.ConfigParser()
+config.read('res/config.ini')
+
+content = """[Settings]
+lang="en"
+"""
+
+try:
+	lang = config['Settings']['lang']
+except:
+	print("Error setting language! Creating new \"config.ini\" file...")
+	with open('res/config.ini', 'w') as file:
+		file.write(content)
+	lang = config['Settings']['lang']
+
+with open(f'res/lang/{lang.replace('"', '')}.json', 'r') as f:
+	data = json.load(f)
+
+name = data["init"]["nameprogram"]
+
+def printname():
+	print(name)
+	print("-" * len(name))
+
+def init():
+	printname()
+
+init()
