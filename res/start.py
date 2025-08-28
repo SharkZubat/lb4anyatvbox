916593e1713815ffb4f2124a683ef26e25c0943c
SharkZubat <akula.puppet.zaba123@gmail.com>
2025-08-28 11:36:29 +0300

    update but requirements.txt is BACK!

--- res/start.py
+++ res/start.py
@@ -1,7 +1,8 @@
 import os
 import configparser
 import json
-import keyboard
+import time
+from colorama import Fore, Back, Style, init
 
 config = configparser.ConfigParser()
 config.read('res/config.ini')
@@ -24,20 +25,106 @@ with open(f'res/lang/{lang.replace('"', '')}.json', 'r') as f:
 	data = json.load(f)
 
 name = data["init"]["nameprogram"]
+init(autoreset=True)
 
 def printname():
-	print(name)
+	print(Style.BRIGHT + name, Style.DIM + data["init"]["version"])
 	print("-" * len(name))
 
+def frame(frame=0):
+	if frame==0:
+		return "-    "
+	elif frame==1:
+		return "=-   "
+	elif frame==2:
+		return "-=-  "
+	elif frame==3:
+		return " -=- "
+	elif frame==4:
+		return "  -=-"
+	elif frame==5:
+		return "   -="
+	elif frame==6:
+		return "    -"
+	elif frame==7:
+		return "     "
+
+def listfiles(isDir):
+	# Get the current working directory as a string
+	current_directory = os.getcwd()
+	# Get a list of files and directories in the current directory
+	file_list = os.listdir(current_directory)
+	
+	folders=[]
+	files=[]
+	
+	# Iterate through each entry in the file list
+	for entry in file_list:
+		# Join the current directory path with the entry name
+		fullpath = os.path.join(current_directory, entry)
+		if os.path.isdir(fullpath):
+			folders.append(entry)
+		else:
+			files.append(entry)
+	
+	if isDir:
+		return folders
+	else:
+		return files
+
+def open():
+#	aframe=0
+#	while True:
+#		framestr=frame(int(aframe%8))
+#		print(framestr, end="\r")
+#		aframe+=1
+#		time.sleep(0.2)
+	#fneakjt
+	os.system("clear")
+	print(Style.BRIGHT + data["filemanager"]["selfile"])
+	print("-"*12)
+	for entry in listfiles(True):
+		print(Fore.LIGHTGREEN_EX + entry)
+	for entry in listfiles(False):
+		print(Fore.WHITE + entry)
+	input(data["filemanager"]["input"])
+
 def main():
-	print(f"(Ctrl+N) {data["project"]["newproj"]}")
-	print(f"(Ctrl+O) {data["project"]["opnproj"]}")
+	print(f"(N) {data["project"]["newproj"]}")
+	print(f"(O) {data["project"]["opnproj"]}")
+	print(f"(E) {data["project"]["exit"]}")
 	#...input...
-	keyboard.add_hotkey('ctrl+n', lambda: print("test success"))
+	try:
+		usrinp=input(">")
+	except KeyboardInterrupt:
+		init()
+	if usrinp=="N" or usrinp=="n":
+		#place holdero
+		#e
+		#a. SFDBGJKSJKTGKDHSSGSKWKHJC
+		#createnew()
+		pass
+	elif usrinp=="O" or usrinp=="o":
+		#eh,m
+		#EHMERO.
+		#EHMMARMOAOMAOIRO....
+		open()
+		pass
+	elif usrinp=="E" or usrinp=="e":
+		exit()
+		pass
+	else:
+		#
+		#
+		init()
+		pass
+	pass
 
 def init():
-	printname()
-	if menu == "main":
-		main()
+	while True:
+		os.system("clear")
+		printname()
+		if menu == "main":
+			main()
 
 init()
